5b5302c5b422447b554f5649db6662d780e08acc
jonia89 <111957252+jonia89@users.noreply.github.com>
2022-10-07 11:47:49 +0300

    Modified config file and wrote functions that won't work :(
    added async and try/catch to functions

--- frontend/src/shoppingCartUtils.js
+++ frontend/src/shoppingCartUtils.js
@@ -1,15 +1,31 @@
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart total.
-function calculateTotal (items) {
-  return 9999;
+async function calculateTotal(items) {
+try {
+  const prices = Object.values(...items)
+  let total = 0;
+  total = prices.match(/\d+/g);
+  total = total.reduce((a, b) => Number(a) + Number(b));
+  return total;
+} catch (error) {
+  console.log("Something went wrong when calculating total", error)
+}
 }
 
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart average.
-function calculateAvg (items) {
-  return 9999;
+async function calculateAvg(items) {
+  try {
+  const prices = Object.values(...items)
+  let average = 0;
+  average = prices.match(/\d+/g);
+  average = average.reduce((a, b) => (Number(a) + Number(b)) / items.length);
+  return average;
+  } catch (error) {
+    console.log("Something went wrong when calculating average", error)
+  }
 }
 
-export { calculateTotal, calculateAvg };
+ export { calculateTotal, calculateAvg };
